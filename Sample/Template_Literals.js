22091fdb29fefdc56f656aba61d183522ef533ab
lkjae <112466589+lkjae@users.noreply.github.com>
2022-08-31 13:36:02 +0900

    Create Template_Literals.js

---
+++ Sample/Template_Literals.js
@@ -0,0 +1,8 @@
+function hello2(name)
+{
+    let name2 = "John";
+    //console.log('Hello' + name2);
+    console.log(`Hello ${name2}. Welcome! ${name}`);// 홑 따옴표X "~"키 에 있는 `
+}
+
+console.log(hello2('유재석'));
