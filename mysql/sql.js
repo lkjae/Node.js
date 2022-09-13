383b44d4ef7f0ae3553466b175c2c4c8b24d4bab
Lee KJ <112466589+lkjae@users.noreply.github.com>
2022-09-13 15:25:33 +0900

    Update sql.js

--- mysql/sql.js
+++ mysql/sql.js
@@ -1,3 +1,5 @@
 module.exports = {
     customerList: `select * from customers`
+    customerInsert: 'insert into customers set ?' // 물음표(?)는 클라이언트에서 전송한 데이터가 오브젝트형인 경우에는 각 키(key)를 컬럼명으로,
+                                                    //값(value)을 컬럼에 들어갈 값으로 변환
 }
