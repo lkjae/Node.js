7a0e72d77dbe0ab861af201cb4f998b0acacb2b2
Lee KJ <112466589+lkjae@users.noreply.github.com>
2022-09-13 15:25:55 +0900

    Update app_mysql.js

--- mysql/app_mysql.js
+++ mysql/app_mysql.js
@@ -5,6 +5,11 @@ const { cursorTo } = require("readline");
 const mysql = require("./mysql"); //mysql 폴더의 index.js
 
 const app = express();
+//환경설정
+app.use(express.json({
+    limit: '50mb' // 최대 50메가
+})); //클라이언트 요청 body를 json으로 파싱 처리
+
 
 app.listen(3000, () => {
     //3000번 포트로 웹 서버 실행
@@ -15,6 +20,7 @@ app.listen(3000, () => {
 app.get('/', function(req,res){
     res.send('Main Page')
 });
+
 app.get('/api/customers', async(req,res) =>{
     //localhost:3000/customers 접속 시 실행
     //sql.js 파일에 작성된 customerList쿼리를 실행
@@ -22,4 +28,10 @@ app.get('/api/customers', async(req,res) =>{
 
     console.log(customers);
     res.send(customers); 
-})
+});
+
+app.post('/api/customer/insert', async(req,res) =>{
+    //삽입 기능
+    const result = await mysql.query('customerInsert', req.body.param);
+    res.send(result);
+});
